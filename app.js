15d6be35364ecdb91957c2ec188c745b0eb1dcb5
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-11-14 17:05:25 +0500

    updated

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const API_KEY = `04ec85a684eae5ff819855d99f118b3b`
-const image_path = `https://image.tmdb.org/t/p/w1280`
+const API_KEY = `42bec90e422952a231200dbc61b8f02b`;
+const image_path = `https://image.tmdb.org/t/p/w1280`;
 
-// - API key will only be available for this video since I'll delete it right after that but you can go to https://www.themoviedb.org/ , create an account and get a key for free.
-// - Image path is only the base url from where we'll get the images.
+const input = document.querySelector(".search input");
+const btn = document.querySelector(".search button");
+const main_grid_title = document.querySelector(".favorites h1");
+const main_grid = document.querySelector(".favorites .movies-grid");
 
-const input = document.querySelector('.search input')
-const btn = document.querySelector('.search button')
-const main_grid_title = document.querySelector('.favorites h1')
-const main_grid = document.querySelector('.favorites .movies-grid')
+const trending_el = document.querySelector(".trending .movies-grid");
 
-const trending_el = document.querySelector('.trending .movies-grid')
+const popup_container = document.querySelector(".popup-container");
 
-const popup_container = document.querySelector('.popup-container')
-
-function add_click_effect_to_card (cards) {
-    cards.forEach(card => {
-        card.addEventListener('click', () => show_popup(card))
-    })
+function add_click_effect_to_card(cards) {
+  cards.forEach((card) => {
+    card.addEventListener("click", () => show_popup(card));
+  });
 }
 
 // SEARCH MOVIES
-async function get_movie_by_search (search_term) {
-    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`)
-    const respData = await resp.json()
-    return respData.results
+async function get_movie_by_search(search_term) {
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`
+  );
+  const respData = await resp.json();
+  return respData.results;
 }
 
-btn.addEventListener('click', add_searched_movies_to_dom)
+btn.addEventListener("click", add_searched_movies_to_dom);
 
-async function add_searched_movies_to_dom () {
-    const data = await get_movie_by_search(input.value)
+async function add_searched_movies_to_dom() {
+  const data = await get_movie_by_search(input.value);
 
-    main_grid_title.innerText = `Search Results...`
-    main_grid.innerHTML = data.map(e => {
-        return `
+  main_grid_title.innerText = `Your Movie Search`;
+  main_grid.innerHTML = data
+    .map((e) => {
+      return `
             <div class="card" data-id="${e.id}">
                 <div class="img">
                     <img src="${image_path + e.poster_path}">
@@ -50,35 +50,42 @@ async function add_searched_movies_to_dom () {
                     </div>
                 </div>
             </div>
-        `
-    }).join('')
+        `;
+    })
+    .join("");
 
-    const cards = document.querySelectorAll('.card')
-    add_click_effect_to_card(cards)
+  const cards = document.querySelectorAll(".card");
+  add_click_effect_to_card(cards);
 }
 
 // POPUP
-async function get_movie_by_id (id) {
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
-    const respData = await resp.json()
-    return respData
+async function get_movie_by_id(id) {
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+  );
+  const respData = await resp.json();
+  return respData;
 }
-async function get_movie_trailer (id) {
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
-    const respData = await resp.json()
-    return respData.results[0].key
+async function get_movie_trailer(id) {
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`
+  );
+  const respData = await resp.json();
+  return respData.results[0].key;
 }
 
-async function show_popup (card) {
-    popup_container.classList.add('show-popup')
+async function show_popup(card) {
+  popup_container.classList.add("show-popup");
 
-    const movie_id = card.getAttribute('data-id')
-    const movie = await get_movie_by_id(movie_id)
-    const movie_trailer = await get_movie_trailer(movie_id)
+  const movie_id = card.getAttribute("data-id");
+  const movie = await get_movie_by_id(movie_id);
+  const movie_trailer = await get_movie_trailer(movie_id);
 
-    popup_container.style.background = `linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 1)), url(${image_path + movie.poster_path})`
+  popup_container.style.background = `linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 1)), url(${
+    image_path + movie.poster_path
+  })`;
 
-    popup_container.innerHTML = `
+  popup_container.innerHTML = `
             <span class="x-icon">&#10006;</span>
             <div class="content">
                 <div class="left">
@@ -118,7 +125,9 @@ async function show_popup (card) {
                     <div class="genres">
                         <h2>Genres</h2>
                         <ul>
-                            ${movie.genres.map(e => `<li>${e.name}</li>`).join('')}
+                            ${movie.genres
+                              .map((e) => `<li>${e.name}</li>`)
+                              .join("")}
                         </ul>
                     </div>
                     <div class="overview">
@@ -131,60 +140,65 @@ async function show_popup (card) {
                     </div>
                 </div>
             </div>
-    `
-    const x_icon = document.querySelector('.x-icon')
-    x_icon.addEventListener('click', () => popup_container.classList.remove('show-popup'))
+    `;
+  const x_icon = document.querySelector(".x-icon");
+  x_icon.addEventListener("click", () =>
+    popup_container.classList.remove("show-popup")
+  );
 
-    const heart_icon = popup_container.querySelector('.heart-icon')
+  const heart_icon = popup_container.querySelector(".heart-icon");
 
-    const movie_ids = get_LS()
-    for(let i = 0; i <= movie_ids.length; i++) {
-        if (movie_ids[i] == movie_id) heart_icon.classList.add('change-color')
+  const movie_ids = get_LS();
+  for (let i = 0; i <= movie_ids.length; i++) {
+    if (movie_ids[i] == movie_id) heart_icon.classList.add("change-color");
+  }
+
+  heart_icon.addEventListener("click", () => {
+    if (heart_icon.classList.contains("change-color")) {
+      remove_LS(movie_id);
+      heart_icon.classList.remove("change-color");
+    } else {
+      add_to_LS(movie_id);
+      heart_icon.classList.add("change-color");
     }
-
-    heart_icon.addEventListener('click', () => {
-        if(heart_icon.classList.contains('change-color')) {
-            remove_LS(movie_id)
-            heart_icon.classList.remove('change-color')
-        } else {
-            add_to_LS(movie_id)
-            heart_icon.classList.add('change-color')
-        }
-        fetch_favorite_movies()
-    })
+    fetch_favorite_movies();
+  });
 }
 
 // Local Storage
-function get_LS () {
-    const movie_ids = JSON.parse(localStorage.getItem('movie-id'))
-    return movie_ids === null ? [] : movie_ids
+function get_LS() {
+  const movie_ids = JSON.parse(localStorage.getItem("movie-id"));
+  return movie_ids === null ? [] : movie_ids;
 }
-function add_to_LS (id) {
-    const movie_ids = get_LS()
-    localStorage.setItem('movie-id', JSON.stringify([...movie_ids, id]))
+function add_to_LS(id) {
+  const movie_ids = get_LS();
+  localStorage.setItem("movie-id", JSON.stringify([...movie_ids, id]));
 }
-function remove_LS (id) {
-    const movie_ids = get_LS()
-    localStorage.setItem('movie-id', JSON.stringify(movie_ids.filter(e => e !== id)))
+function remove_LS(id) {
+  const movie_ids = get_LS();
+  localStorage.setItem(
+    "movie-id",
+    JSON.stringify(movie_ids.filter((e) => e !== id))
+  );
 }
 
 // Favorite Movies
-fetch_favorite_movies()
-async function fetch_favorite_movies () {
-    main_grid.innerHTML = ''
+fetch_favorite_movies();
+async function fetch_favorite_movies() {
+  main_grid.innerHTML = "";
 
-    const movies_LS = await get_LS()
-    const movies = []
-    for(let i = 0; i <= movies_LS.length - 1; i++) {
-        const movie_id = movies_LS[i]
-        let movie = await get_movie_by_id(movie_id)
-        add_favorites_to_dom_from_LS(movie)
-        movies.push(movie)
-    }
+  const movies_LS = await get_LS();
+  const movies = [];
+  for (let i = 0; i <= movies_LS.length - 1; i++) {
+    const movie_id = movies_LS[i];
+    let movie = await get_movie_by_id(movie_id);
+    add_favorites_to_dom_from_LS(movie);
+    movies.push(movie);
+  }
 }
 
-function add_favorites_to_dom_from_LS (movie_data) {
-    main_grid.innerHTML += `
+function add_favorites_to_dom_from_LS(movie_data) {
+  main_grid.innerHTML += `
         <div class="card" data-id="${movie_data.id}">
             <div class="img">
                 <img src="${image_path + movie_data.poster_path}">
@@ -201,28 +215,31 @@ function add_favorites_to_dom_from_LS (movie_data) {
                 </div>
             </div>
         </div>
-    `
+    `;
 
-    const cards = document.querySelectorAll('.card')
-    add_click_effect_to_card(cards)
+  const cards = document.querySelectorAll(".card");
+  add_click_effect_to_card(cards);
 }
 
 // Trending Movies
-get_trending_movies()
-async function get_trending_movies () {
-    const resp = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
-    const respData = await resp.json()
-    return respData.results
+get_trending_movies();
+async function get_trending_movies() {
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+  );
+  const respData = await resp.json();
+  return respData.results;
 }
 
-add_to_dom_trending()
-async function add_to_dom_trending () {
+add_to_dom_trending();
+async function add_to_dom_trending() {
+  const data = await get_trending_movies();
+  console.log(data);
 
-    const data = await get_trending_movies()
-    console.log(data);
-
-    trending_el.innerHTML = data.slice(0, 5).map(e => {
-        return `
+  trending_el.innerHTML = data
+    .slice(0, 5)
+    .map((e) => {
+      return `
             <div class="card" data-id="${e.id}">
                 <div class="img">
                     <img src="${image_path + e.poster_path}">
@@ -239,6 +256,7 @@ async function add_to_dom_trending () {
                     </div>
                 </div>
             </div>
-        `
-    }).join('')
+        `;
+    })
+    .join("");
 }
